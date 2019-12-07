265fcc6732f16da8dc6459b8252585604a51de02
Xtremilicious <nilarjundas@outlook.com>
2019-12-07 20:54:25 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/MainPage'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+          </div>
+        </div>
+      </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
-
 
 const store = configureStore();
 
@@ -14,7 +17,7 @@ const jsx = (
     </Provider>
   );
 
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
