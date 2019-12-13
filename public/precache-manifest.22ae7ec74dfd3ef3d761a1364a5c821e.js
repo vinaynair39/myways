9f1e46b580268a5ad9e616aff9ab5bd15e91b714
vinaynair39 <vnnair39@gmail.com>
2019-12-13 17:28:10 +0530

    package.json errr ofixed

---
+++ public/precache-manifest.22ae7ec74dfd3ef3d761a1364a5c821e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce10bf8ef81e55dafb7121326265650f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb2e6c34469d52c3143e",
+    "url": "/static/css/2.31f0f9a1.chunk.css"
+  },
+  {
+    "revision": "488e5221fc879a3b8549",
+    "url": "/static/css/main.86b5b918.chunk.css"
+  },
+  {
+    "revision": "fb2e6c34469d52c3143e",
+    "url": "/static/js/2.aec47b37.chunk.js"
+  },
+  {
+    "revision": "65f53bb319e84c3f6ed28d244519e52d",
+    "url": "/static/js/2.aec47b37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "488e5221fc879a3b8549",
+    "url": "/static/js/main.db25218f.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "6148168cfa71bae0edcc9ede54ebc8de",
+    "url": "/static/media/pic.6148168c.png"
+  }
+]);
