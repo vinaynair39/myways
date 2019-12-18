83c0b9e035d1b7124663bb43614d2bb6b02b488a
vinaynair39 <vnnair39@gmail.com>
2019-12-18 18:27:51 +0530

    chartbox added

--- client/src/components/result/ResultChart.js
+++ client/src/components/result/ResultChart.js
@@ -5,10 +5,13 @@ import ResultChartBox from './ResultChartBox'
 
 const ResultChart = ({ results }) => (
     <>
-        <div className="row d-flex justify-content-center" style={{ width: "80vw" }}>
+        <div className="row d-flex justify-content-center" style={{ width: "100vw" }}>
         </div>
-        <div className="row d-flex justify-content-center" style={{ width: "80vw" }}>
-        <ResultChartBox/>
+        <div className="row d-flex justify-content-center" style={{ width: "100vw" }}>
+            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491416.png"} colr="#FACC2E"/></div>
+        
+            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/svg/1301/1301414.svg"} colr="#F25C5C"/></div>
+        <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491376.png"} colr="#0080FF"/></div>
         </div>
     </>
 )
