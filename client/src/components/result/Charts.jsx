d12faabc013f0f95551f2feafad364e460dd869d
vinaynair39 <vnnair39@gmail.com>
2019-12-18 21:46:21 +0530

    before stashing

---
+++ client/src/components/result/Charts.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from 'react';
+import Chart from 'react-apexcharts';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";  
+
+
+const BarChart = () => {
+    const [options, setOptions] = useState({})
+    const [series, setSeries] = useState([]);
+
+    useEffect(() => {
+        setOptions({
+            chart: {
+                toolbar: false
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    distributed: true,
+                }
+            },
+            colors: ['#FACC2E', '#F25C5C', '#0080FF'],
+            grid: {
+                xaxis: {
+                    lines: {
+                        show: true
+                    }
+                },
+                yaxis: {
+                    lines: {
+                        show: false
+                    }
+                }
+            },
+
+            xaxis: {
+
+                type: 'category',
+                categories: ['Thinking', 'Feeling', 'Intuition'],
+                labels: {
+                    show: true,
+                    rotate: -45,
+                    rotateAlways: false,
+                    hideOverlappingLabels: true,
+                    showDuplicates: false,
+                    trim: true,
+                    minHeight: undefined,
+                    maxHeight: 120,
+                    style: {
+                        colors: [],
+                        fontSize: '15px',
+                        fontFamily: 'Helvetica, Arial, sans-serif',
+                        cssClass: 'apexcharts-xaxis-label',
+                    },
+                    offsetX: 0,
+                    offsetY: 0,
+                    format: undefined,
+                    formatter: undefined,
+                    datetimeFormatter: {
+                        year: 'yyyy',
+                        month: "MMM 'yy",
+                        day: 'dd MMM',
+                        hour: 'HH:mm',
+                    },
+                },
+                axisBorder: {
+                    show: true,
+                    color: '#78909C',
+                    height: 1,
+                    width: '100%',
+                    offsetX: 0,
+                    offsetY: 0
+                },
+                axisTicks: {
+                    show: true,
+                    borderType: 'solid',
+                    color: '#78909C',
+                    height: 6,
+                    offsetX: 0,
+                    offsetY: 0
+                },
+                tickAmount: undefined,
+                tickPlacement: 'between',
+                min: undefined,
+                max: undefined,
+                range: undefined,
+                floating: false,
+                position: 'bottom',
+                title: {
+                    text: undefined,
+                    offsetX: 0,
+                    offsetY: 0,
+                    style: {
+                        color: undefined,
+                        fontSize: '12px',
+                        fontFamily: 'Helvetica, Arial, sans-serif',
+                        cssClass: 'apexcharts-xaxis-title',
+                    },
+                },
+                crosshairs: {
+                    show: true,
+                    width: 1,
+                    position: 'back',
+                    opacity: 0.9,
+                    stroke: {
+                        color: '#b6b6b6',
+                        width: 0,
+                        dashArray: 0,
+                    },
+                    fill: {
+                        type: 'solid',
+                        color: '#B1B9C4',
+                        gradient: {
+                            colorFrom: '#D8E3F0',
+                            colorTo: '#BED1E6',
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5,
+                        },
+                    },
+                    dropShadow: {
+                        enabled: false,
+                        top: 0,
+                        left: 0,
+                        blur: 1,
+                        opacity: 0.4,
+                    },
+                },
+                tooltip: {
+                    enabled: true,
+                    formatter: undefined,
+                    offsetY: 0,
+                    style: {
+                        fontSize: 0,
+                        fontFamily: 0,
+                    },
+                },
+                
+            }
+
+        });
+        setSeries([{
+            data: [90, 60, 40]
+        }])
+    }, [])
+
+    return (
+        <div id="chart">
+            <Chart options={options} series={series} type="bar" height="350" width="70%" />
+        </div>
+    );
+}
+
+export default BarChart;
+
--- client/src/components/result/ResultChart.js
+++ client/src/components/result/ResultChart.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ResultChartBox from './ResultChartBox'
+import ResultChartBox from './ResultChartBox';
+import Charts from './Charts'
 // import { faPlus } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";   
 
 const ResultChart = ({ results }) => (
     <>
+        <Charts />
         <div className="row d-flex justify-content-center" style={{ width: "100vw" }}>
         </div>
         <div className="row d-flex justify-content-center" style={{ width: "100vw" }}>
-            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491416.png"} colr="#FACC2E"/></div>
-        
-            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/svg/1301/1301414.svg"} colr="#F25C5C"/></div>
-        <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491376.png"} colr="#0080FF"/></div>
+            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491416.png"} colr="#FACC2E" /></div>
+
+            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/svg/1301/1301414.svg"} colr="#F25C5C" /></div>
+            <div className="col-4"><ResultChartBox img={"https://image.flaticon.com/icons/png/512/1491/1491376.png"} colr="#0080FF" /></div>
         </div>
     </>
 )
